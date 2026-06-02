4b6ea6353ac8ac14cb78e757a7ff8f3bab8fd1b7
David <martindavid99@gmail.com>
2026-06-01 23:09:57 -0500

    adding a click event

--- src/App.jsx
+++ src/App.jsx
@@ -37,8 +37,12 @@ function Accordion({ data }) {
 function AccordionItem({ num, title, text }) {
   const [isOpen, setIsOpen] = useState(false);
 
+  function handleClick() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
   return (
-    <div className="item">
+    <div className="item" onClick={handleClick}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="text">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
